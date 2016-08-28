8116998d78b552d3ff000d4be2f8e755adc1fffd
Raul Naupari <raul.naupari@cignium.com>
2016-08-28 16:31:39 -0500

    lightinject implementation

---
+++ Jal.Locator.LightInject/Impl/ServiceLocator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using Jal.Locator.Interface;
+using LightInject;
+
+namespace Jal.Locator.LightInject.Impl
+{
+    public class ServiceLocator : IServiceLocator
+    {
+        private readonly IServiceContainer _container;
+
+        public ServiceLocator(IServiceContainer container)
+        {
+            _container = container;
+        }
+
+        public TSource Resolve<TSource>() where TSource : class
+        {
+            return _container.GetInstance<TSource>();
+        }
+
+        public TSource Resolve<TSource>(string key) where TSource : class
+        {
+            return _container.GetInstance<TSource>(key);
+        }
+
+        public TSource[] ResolveAll<TSource>() where TSource : class
+        {
+            var allInstances = _container.GetAllInstances<TSource>();
+
+            if (allInstances != null)
+            {
+                return allInstances.ToArray();
+            }
+            else
+            {
+                return null;
+            }               
+        }
+
+        public object Resolve(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        public object Resolve(Type service)
+        {
+            return _container.GetInstance(service);
+        }
+    }
+}
--- pack.bat
+++ pack.bat
@@ -1,5 +1,7 @@
-packages\NuGet.CommandLine.2.8.6\tools\nuget pack Jal.Locator\Jal.Locator.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Locator.Nuget
+packages\NuGet.CommandLine.3.4.4-rtm-final\tools\nuget pack Jal.Locator\Jal.Locator.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Locator.Nuget
 
-packages\NuGet.CommandLine.2.8.6\tools\nuget pack Jal.Locator.CastleWindsor\Jal.Locator.CastleWindsor.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Locator.Nuget
+packages\NuGet.CommandLine.3.4.4-rtm-final\tools\nuget pack Jal.Locator.CastleWindsor\Jal.Locator.CastleWindsor.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Locator.Nuget
+
+packages\NuGet.CommandLine.3.4.4-rtm-final\tools\nuget pack Jal.Locator.LightInject\Jal.Locator.LightInject.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Locator.Nuget
 
 pause;
